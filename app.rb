3598391a98fde52ae32b72b83358a75ec774a1e3
Deepa Kackar <dkackar99@yahoo.com>
2016-01-25 09:00:23 -0800

    added rspec

--- app.rb
+++ app.rb
@@ -1,5 +1,28 @@
 require 'sinatra'
 require_relative 'lib/tower_of_hanoi'
+require 'json'
 
 enable :sessions
+
+ get '/' do 
+
+  toh = TowerOfHanoi.new
+  session[:game_state] = toh.towers.to_json
+  towers = toh.towers
+
+  erb :game, :locals => {:towers => towers}
+   
+ end
+
+
+ post '/' do 
+
+  towers = JSON.parse(session[:game_state])
+  toh = TowerOfHanoi.new(towers)
+  
+  session[:towers] = toh.towers.to_json
+
+  erb :game, :locals => {:towers => towers}
+   
+ end
 
